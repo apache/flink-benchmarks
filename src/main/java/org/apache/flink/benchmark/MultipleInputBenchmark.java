0d25602a4c7b716e245230f028af95cad1373345
Piotr Nowojski <piotr.nowojski@gmail.com>
2020-02-21 16:36:05 +0100

    [FLINK-18905] Provide basic benchmarks for MultipleInputStreamOperator

--- src/main/java/org/apache/flink/benchmark/MultipleInputBenchmark.java
+++ src/main/java/org/apache/flink/benchmark/MultipleInputBenchmark.java
@@ -18,13 +18,15 @@
 
 package org.apache.flink.benchmark;
 
-import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.benchmark.functions.LongSource;
 import org.apache.flink.benchmark.functions.QueuingLongSource;
-import org.apache.flink.benchmark.operators.MultiplyByTwoCoStreamMap;
+import org.apache.flink.benchmark.operators.MultiplyByTwoOperatorFactory;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.MultipleConnectedStreams;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.DiscardingSink;
+import org.apache.flink.streaming.api.transformations.MultipleInputTransformation;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.OperationsPerInvocation;
@@ -34,65 +36,68 @@ import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.runner.options.VerboseMode;
 
-public class TwoInputBenchmark extends BenchmarkBase {
+public class MultipleInputBenchmark extends BenchmarkBase {
 
-	public static final int RECORDS_PER_INVOCATION = 25_000_000;
-	public static final int ONE_IDLE_RECORDS_PER_INVOCATION = 15_000_000;
-	public static final long CHECKPOINT_INTERVAL_MS = 100;
+	public static final int RECORDS_PER_INVOCATION = TwoInputBenchmark.RECORDS_PER_INVOCATION;
+	public static final int ONE_IDLE_RECORDS_PER_INVOCATION = TwoInputBenchmark.ONE_IDLE_RECORDS_PER_INVOCATION;
+	public static final long CHECKPOINT_INTERVAL_MS = TwoInputBenchmark.CHECKPOINT_INTERVAL_MS;
 
 	public static void main(String[] args)
 		throws RunnerException {
 		Options options = new OptionsBuilder()
 			.verbosity(VerboseMode.NORMAL)
-			.include(".*" + TwoInputBenchmark.class.getCanonicalName() + ".*")
+			.include(".*" + MultipleInputBenchmark.class.getSimpleName() + ".*")
 			.build();
 
 		new Runner(options).run();
 	}
 
 	@Benchmark
-	@OperationsPerInvocation(value = TwoInputBenchmark.RECORDS_PER_INVOCATION)
-	public void twoInputMapSink(FlinkEnvironmentContext context) throws Exception {
+	@OperationsPerInvocation(RECORDS_PER_INVOCATION)
+	public void multiInputMapSink(FlinkEnvironmentContext context) throws Exception {
 
 		StreamExecutionEnvironment env = context.env;
-
 		env.enableCheckpointing(CHECKPOINT_INTERVAL_MS);
-		env.setParallelism(1);
-
-		// Setting buffer timeout to 1 is an attempt to improve twoInputMapSink benchmark stability.
-		// Without 1ms buffer timeout, some JVM forks are much slower then others, making results
-		// unstable and unreliable.
-		env.setBufferTimeout(1);
 
 		long numRecordsPerInput = RECORDS_PER_INVOCATION / 2;
 		DataStreamSource<Long> source1 = env.addSource(new LongSource(numRecordsPerInput));
 		DataStreamSource<Long> source2 = env.addSource(new LongSource(numRecordsPerInput));
-
-		source1
-			.connect(source2)
-			.transform("custom operator", TypeInformation.of(Long.class), new MultiplyByTwoCoStreamMap())
-			.addSink(new DiscardingSink<>());
+		connectAndDiscard(env, source1, source2);
 
 		env.execute();
 	}
 
 	@Benchmark
-	@OperationsPerInvocation(value = TwoInputBenchmark.ONE_IDLE_RECORDS_PER_INVOCATION)
-	public void twoInputOneIdleMapSink(FlinkEnvironmentContext context) throws Exception {
+	@OperationsPerInvocation(ONE_IDLE_RECORDS_PER_INVOCATION)
+	public void multiInputOneIdleMapSink(FlinkEnvironmentContext context) throws Exception {
 
 		StreamExecutionEnvironment env = context.env;
 		env.enableCheckpointing(CHECKPOINT_INTERVAL_MS);
-		env.setParallelism(1);
 
 		QueuingLongSource.reset();
 		DataStreamSource<Long> source1 = env.addSource(new QueuingLongSource(1, ONE_IDLE_RECORDS_PER_INVOCATION - 1));
 		DataStreamSource<Long> source2 = env.addSource(new QueuingLongSource(2, 1));
-
-		source1
-				.connect(source2)
-				.transform("custom operator", TypeInformation.of(Long.class), new MultiplyByTwoCoStreamMap())
-				.addSink(new DiscardingSink<>());
+		connectAndDiscard(env, source1, source2);
 
 		env.execute();
+	}
+
+	private static void connectAndDiscard(
+			StreamExecutionEnvironment env,
+			DataStreamSource<Long> source1,
+			DataStreamSource<Long> source2) {
+		MultipleInputTransformation<Long> transform = new MultipleInputTransformation<>(
+				"custom operator",
+				new MultiplyByTwoOperatorFactory(),
+				BasicTypeInfo.LONG_TYPE_INFO,
+				1);
+
+		transform.addInput(source1.getTransformation());
+		transform.addInput(source2.getTransformation());
+
+		env.addOperator(transform);
+		new MultipleConnectedStreams(env)
+				.transform(transform)
+				.addSink(new DiscardingSink<>());
 	}
 }
