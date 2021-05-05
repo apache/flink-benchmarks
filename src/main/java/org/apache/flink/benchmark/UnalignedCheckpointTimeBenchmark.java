9cf75d5115cfe40a73724c6e5a22c48b4c8e403d
Piotr Nowojski <piotr.nowojski@gmail.com>
2021-05-05 15:01:28 +0200

    [FLINK-22973] Provide unaligned checkpoint with timeouts benchmark

--- src/main/java/org/apache/flink/benchmark/UnalignedCheckpointTimeBenchmark.java
+++ src/main/java/org/apache/flink/benchmark/UnalignedCheckpointTimeBenchmark.java
@@ -19,14 +19,15 @@
 package org.apache.flink.benchmark;
 
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.benchmark.operators.RecordSource;
+import org.apache.flink.benchmark.operators.RecordSource.Record;
 import org.apache.flink.configuration.ConfigConstants;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.MemorySize;
 import org.apache.flink.configuration.TaskManagerOptions;
-import org.apache.flink.runtime.state.CheckpointListener;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
-import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.OperationsPerInvocation;
@@ -42,45 +43,56 @@ import org.openjdk.jmh.runner.options.VerboseMode;
 import java.io.IOException;
 import java.time.Duration;
 
-import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.apache.flink.api.common.eventtime.WatermarkStrategy.noWatermarks;
 
 /**
- * The benchmark for measuring the time taken to finish the configured number of
- * unaligned checkpoints.
+ * The benchmark for measuring the time taken to finish the configured number of unaligned
+ * checkpoints.
  */
-@OutputTimeUnit(MINUTES)
-@OperationsPerInvocation(value = UnalignedCheckpointTimeBenchmark.NUM_FINISHED_CHECKPOINTS)
+@OutputTimeUnit(SECONDS)
+@OperationsPerInvocation(UnalignedCheckpointTimeBenchmark.NUM_FINISHED_CHECKPOINTS)
 public class UnalignedCheckpointTimeBenchmark extends BenchmarkBase {
-    public static final int NUM_FINISHED_CHECKPOINTS = 5;
+    public static final int NUM_FINISHED_CHECKPOINTS = 10;
     private static final int NUM_VERTICES = 3;
     private static final int PARALLELISM = 4;
     private static final long CHECKPOINT_INTERVAL_MS = 10;
 
     public static void main(String[] args) throws RunnerException {
-        Options options = new OptionsBuilder()
-            .verbosity(VerboseMode.NORMAL)
-            .include(UnalignedCheckpointTimeBenchmark.class.getCanonicalName())
-            .build();
+        Options options =
+                new OptionsBuilder()
+                        .verbosity(VerboseMode.NORMAL)
+                        .include(UnalignedCheckpointTimeBenchmark.class.getCanonicalName())
+                        .build();
 
         new Runner(options).run();
     }
 
     @Benchmark
-    public void unalignedCheckpoint(UnalignedCheckpointEnvironmentContext context) throws Exception {
+    public void unalignedCheckpoint(UnalignedCheckpointEnvironmentContext context)
+            throws Exception {
         StreamExecutionEnvironment env = context.env;
-        DataStreamSource<byte[]> source = env.addSource(new FiniteCheckpointSource(NUM_FINISHED_CHECKPOINTS));
-        source
-            .slotSharingGroup("source").rebalance()
-            .map((MapFunction<byte[], byte[]>) value -> value).slotSharingGroup("map").rebalance()
-            .addSink(new SlowDiscardSink<>()).slotSharingGroup("sink");
+        DataStreamSource<Record> source =
+                env.fromSource(
+                        new RecordSource(NUM_FINISHED_CHECKPOINTS),
+                        noWatermarks(),
+                        RecordSource.class.getName());
+
+        source.slotSharingGroup("source")
+                .rebalance()
+                .map((MapFunction<Record, Record>) value -> value)
+                .slotSharingGroup("map")
+                .rebalance()
+                .addSink(new SlowDiscardSink<>())
+                .slotSharingGroup("sink");
 
         env.execute();
     }
 
     public static class UnalignedCheckpointEnvironmentContext extends FlinkEnvironmentContext {
 
-        @Param({"REMOTE", "LOCAL"})
-        public String mode = "REMOTE";
+        @Param({"0", "1", "ALIGNED"})
+        public String timeout = "0";
 
         @Setup
         public void setUp() throws IOException {
@@ -88,73 +100,33 @@ public class UnalignedCheckpointTimeBenchmark extends BenchmarkBase {
 
             env.setParallelism(parallelism);
             env.enableCheckpointing(CHECKPOINT_INTERVAL_MS);
-            env.getCheckpointConfig().enableUnalignedCheckpoints(true);
-            env.getCheckpointConfig().setAlignmentTimeout(Duration.ZERO);
+            if ("ALIGNED".equals(timeout)) {
+                env.getCheckpointConfig().enableUnalignedCheckpoints(false);
+            } else {
+                env.getCheckpointConfig().enableUnalignedCheckpoints(true);
+                env.getCheckpointConfig()
+                        .setAlignmentTimeout(Duration.ofMillis(Integer.parseInt(timeout)));
+            }
         }
 
         protected Configuration createConfiguration() {
             Configuration conf = super.createConfiguration();
-
-            if (mode.equals("REMOTE")) {
-                conf.setInteger(TaskManagerOptions.NUM_TASK_SLOTS, 1);
-                conf.setInteger(ConfigConstants.LOCAL_NUMBER_TASK_MANAGER, NUM_VERTICES * PARALLELISM);
-            } else {
-                conf.setInteger(TaskManagerOptions.NUM_TASK_SLOTS, NUM_VERTICES * PARALLELISM);
-                conf.setInteger(ConfigConstants.LOCAL_NUMBER_TASK_MANAGER, 1);
-            }
+            conf.setInteger(TaskManagerOptions.NUM_TASK_SLOTS, 1);
+            conf.set(TaskManagerOptions.MEMORY_SEGMENT_SIZE, new MemorySize(1024 * 4));
+            conf.setInteger(ConfigConstants.LOCAL_NUMBER_TASK_MANAGER, NUM_VERTICES * PARALLELISM);
             return conf;
         }
     }
 
     /**
-     * The source for finishing the configured number of checkpoints before exiting.
-     */
-    public static class FiniteCheckpointSource extends RichParallelSourceFunction<byte[]> implements CheckpointListener {
-
-        private final int numExpectedCheckpoints;
-        private final byte[] bytes = new byte[1024];
-
-        private volatile boolean running = true;
-        private volatile int numFinishedCheckpoints;
-
-        FiniteCheckpointSource(int numCheckpoints) {
-            this.numExpectedCheckpoints = numCheckpoints;
-        }
-
-        @Override
-        public void notifyCheckpointComplete(long checkpointId) {
-            ++numFinishedCheckpoints;
-        }
-
-
-        @Override
-        public void run(SourceContext<byte[]> ctx) {
-            while (running) {
-                synchronized (ctx.getCheckpointLock()) {
-                    ctx.collect(bytes);
-
-                    if (numFinishedCheckpoints >= numExpectedCheckpoints) {
-                        cancel();
-                    }
-                }
-            }
-        }
-
-        @Override
-        public void cancel() {
-            running = false;
-        }
-    }
-
-    /**
-     * The custom sink for processing records slowly to cause accumulate in-flight
-     * buffers even back pressure.
+     * The custom sink for processing records slowly to cause accumulate in-flight buffers even back
+     * pressure.
      */
     public static class SlowDiscardSink<T> implements SinkFunction<T> {
 
         @Override
-        public void invoke(T value, SinkFunction.Context context) throws Exception {
-            Thread.sleep(3);
+        public void invoke(T value, Context context) throws Exception {
+            Thread.sleep(1);
         }
     }
 }
