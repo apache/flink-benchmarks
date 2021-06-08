b1db51067452efa2a1f0454f48b87f641170234b
Dawid Wysakowicz <dwysakowicz@apache.org>
2021-06-08 14:45:04 +0200

    [FLINK-22881] Parameterize InputBenchmark test to use FLIP-27 source

---
+++ src/main/java/org/apache/flink/benchmark/functions/LongNewSource.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.benchmark.functions;
+
+import org.apache.flink.api.connector.source.Boundedness;
+import org.apache.flink.api.connector.source.SplitEnumerator;
+import org.apache.flink.api.connector.source.SplitEnumeratorContext;
+import org.apache.flink.api.connector.source.lib.NumberSequenceSource;
+import org.apache.flink.api.connector.source.lib.util.IteratorSourceEnumerator;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * The source should produce same records as {@link LongSource}.
+ */
+public class LongNewSource extends NumberSequenceSource {
+	private final Boundedness boundedness;
+	private final long maxValue;
+
+	public LongNewSource(Boundedness boundedness, long maxValue) {
+		super(-1, -1); // we do not use the from/to of the underlying source
+		this.boundedness = boundedness;
+		this.maxValue = maxValue;
+	}
+
+	@Override
+	public Boundedness getBoundedness() {
+		return boundedness;
+	}
+
+	@Override
+	public SplitEnumerator<NumberSequenceSplit, Collection<NumberSequenceSplit>> createEnumerator(
+			SplitEnumeratorContext<NumberSequenceSplit> splitEnumeratorContext) {
+
+		final List<NumberSequenceSplit> splits =
+				IntStream.range(0, splitEnumeratorContext.currentParallelism())
+						.mapToObj(
+								id -> new NumberSequenceSplit(String.valueOf(id), 0, maxValue)
+						)
+						.collect(Collectors.toList());
+		return new IteratorSourceEnumerator<>(splitEnumeratorContext, splits);
+	}
+}
---
+++ src/main/java/org/apache/flink/benchmark/functions/LongSourceType.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.benchmark.functions;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.connector.source.Boundedness;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.function.BiFunction;
+
+/**
+ * Enum based factory for different Long sources.
+ */
+public enum LongSourceType {
+	LEGACY_SOURCE((env, maxValue) -> {
+		return env.addSource(new LongSource(maxValue));
+	}),
+	FLIP_27_SOURCE_BOUNDED((env, maxValue) -> {
+		return env.fromSource(
+				new LongNewSource(Boundedness.BOUNDED, maxValue),
+				WatermarkStrategy.noWatermarks(),
+				"NewLongSource");
+	}),
+	FLIP_27_SOURCE_UNBOUNDED((env, maxValue) -> {
+		return env.fromSource(
+				new LongNewSource(Boundedness.BOUNDED, maxValue),
+				WatermarkStrategy.noWatermarks(),
+				"NewLongSource");
+	});
+	private final BiFunction<StreamExecutionEnvironment, Long, DataStreamSource<Long>> factory;
+
+	LongSourceType(BiFunction<StreamExecutionEnvironment, Long, DataStreamSource<Long>> factory) {
+		this.factory = factory;
+	}
+
+	public DataStreamSource<Long> source(StreamExecutionEnvironment environment, long maxValue) {
+		return factory.apply(environment, maxValue);
+	}
+};
