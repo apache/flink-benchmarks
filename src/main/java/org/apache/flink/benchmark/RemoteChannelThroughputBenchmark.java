ee295fbfec550529cd029b23b0ce1ba9259c18d4
Piotr Nowojski <piotr.nowojski@gmail.com>
2021-10-07 12:33:52 +0200

    [FLINK-24471] Add microbenchmark for throughput with debloating enabled

--- src/main/java/org/apache/flink/benchmark/RemoteChannelThroughputBenchmark.java
+++ src/main/java/org/apache/flink/benchmark/RemoteChannelThroughputBenchmark.java
@@ -20,6 +20,8 @@ package org.apache.flink.benchmark;
 
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.benchmark.functions.LongSource;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.TaskManagerOptions;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.DiscardingSink;
@@ -28,6 +30,8 @@ import org.apache.flink.streaming.api.graph.StreamingJobGraphGenerator;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.OperationsPerInvocation;
 import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -36,18 +40,19 @@ import org.openjdk.jmh.runner.options.VerboseMode;
 
 @OperationsPerInvocation(value = RemoteChannelThroughputBenchmark.RECORDS_PER_INVOCATION)
 public class RemoteChannelThroughputBenchmark extends RemoteBenchmarkBase {
+    private static final String ALIGNED = "ALIGNED";
+    private static final String DEBLOAT = "DEBLOAT";
+    private static final String UNALIGNED = "UNALIGNED";
+
     private static final int NUM_VERTICES = 3;
     private static final long CHECKPOINT_INTERVAL_MS = 100;
 
-    @Param({"ALIGNED", "UNALIGNED"})
-    public String mode = "ALIGNED";
-
-    public static void main(String[] args)
-            throws RunnerException {
-        Options options = new OptionsBuilder()
-                .verbosity(VerboseMode.NORMAL)
-                .include(RemoteChannelThroughputBenchmark.class.getCanonicalName())
-                .build();
+    public static void main(String[] args) throws RunnerException {
+        Options options =
+                new OptionsBuilder()
+                        .verbosity(VerboseMode.NORMAL)
+                        .include(RemoteChannelThroughputBenchmark.class.getCanonicalName())
+                        .build();
 
         new Runner(options).run();
     }
@@ -58,18 +63,37 @@ public class RemoteChannelThroughputBenchmark extends RemoteBenchmarkBase {
     }
 
     @Benchmark
-    public void remoteRebalance(FlinkEnvironmentContext context) throws Exception {
+    public void remoteRebalance(RemoteChannelThroughputBenchmarkContext context) throws Exception {
         StreamExecutionEnvironment env = context.env;
         env.enableCheckpointing(CHECKPOINT_INTERVAL_MS);
         env.setParallelism(PARALLELISM);
-        env.getCheckpointConfig().enableUnalignedCheckpoints(!mode.equals("ALIGNED"));
+        env.getCheckpointConfig().enableUnalignedCheckpoints(context.mode.equals(UNALIGNED));
 
         DataStreamSource<Long> source = env.addSource(new LongSource(RECORDS_PER_SUBTASK));
-        source
-            .slotSharingGroup("source").rebalance()
-            .map((MapFunction<Long, Long>) value -> value).slotSharingGroup("map").rebalance()
-            .addSink(new DiscardingSink<>()).slotSharingGroup("sink");
+        source.slotSharingGroup("source")
+                .rebalance()
+                .map((MapFunction<Long, Long>) value -> value)
+                .slotSharingGroup("map")
+                .rebalance()
+                .addSink(new DiscardingSink<>())
+                .slotSharingGroup("sink");
 
-        miniCluster.executeJobBlocking(StreamingJobGraphGenerator.createJobGraph(env.getStreamGraph()));
+        miniCluster.executeJobBlocking(
+                StreamingJobGraphGenerator.createJobGraph(env.getStreamGraph()));
+    }
+
+    @State(Scope.Thread)
+    public static class RemoteChannelThroughputBenchmarkContext extends FlinkEnvironmentContext {
+        @Param({ALIGNED, UNALIGNED, DEBLOAT})
+        public String mode = ALIGNED;
+
+        @Override
+        protected Configuration createConfiguration() {
+            Configuration configuration = super.createConfiguration();
+            if (mode.equals(DEBLOAT)) {
+                configuration.setBoolean(TaskManagerOptions.BUFFER_DEBLOAT_ENABLED, true);
+            }
+            return configuration;
+        }
     }
 }
