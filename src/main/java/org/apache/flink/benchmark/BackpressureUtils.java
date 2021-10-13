8a1d191eb10fd8bc2ad636c9e6fa35a0b15dc6ce
Piotr Nowojski <piotr.nowojski@gmail.com>
2021-10-13 12:30:55 +0200

    [FLINK-24531] Fix connection time out when checking the backpressure

--- src/main/java/org/apache/flink/benchmark/BackpressureUtils.java
+++ src/main/java/org/apache/flink/benchmark/BackpressureUtils.java
@@ -20,35 +20,39 @@ package org.apache.flink.benchmark;
 
 import org.apache.flink.api.common.JobID;
 import org.apache.flink.api.common.time.Deadline;
+import org.apache.flink.client.deployment.StandaloneClusterId;
+import org.apache.flink.client.program.rest.RestClusterClient;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.configuration.UnmodifiableConfiguration;
+import org.apache.flink.configuration.RestOptions;
 import org.apache.flink.runtime.jobgraph.JobVertexID;
-import org.apache.flink.runtime.rest.RestClient;
 import org.apache.flink.runtime.rest.messages.EmptyRequestBody;
 import org.apache.flink.runtime.rest.messages.JobVertexBackPressureHeaders;
 import org.apache.flink.runtime.rest.messages.JobVertexBackPressureInfo;
 import org.apache.flink.runtime.rest.messages.JobVertexMessageParameters;
 import org.apache.flink.util.FlinkRuntimeException;
-import org.apache.flink.util.concurrent.ExecutorThreadFactory;
 
 import java.net.URI;
 import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.Executors;
 
-/**
- * Utility class for querying a backpressure status.
- */
+/** Utility class for querying a backpressure status. */
 public class BackpressureUtils {
-    static void waitForBackpressure(JobID jobID, List<JobVertexID> sourceId, URI restAddress)
+
+    static void waitForBackpressure(
+            JobID jobID,
+            List<JobVertexID> sourceId,
+            URI restAddress,
+            Configuration clientConfiguration)
             throws Exception {
-        final RestClient restClient =
-                new RestClient(
-                        new UnmodifiableConfiguration(new Configuration()),
-                        Executors.newSingleThreadScheduledExecutor(
-                                new ExecutorThreadFactory("Flink-RestClient-IO")));
+        RestClusterClient<StandaloneClusterId> restClient =
+                createClient(restAddress.getPort(), clientConfiguration);
         Deadline deadline = Deadline.fromNow(Duration.ofSeconds(30));
         boolean allBackpressured;
+        // There seems to be a race condition in some setups, between setting up REST server
+        // and client being able to connect. This is handled by the retrying mechanism in
+        // the RestClusterClient, but time out takes a lot of time to trigger, so we are
+        // doing a little bit of sleep here in an attempt to avoid waiting for that timeout.
+        Thread.sleep(100);
         do {
             allBackpressured =
                     sourceId.stream()
@@ -65,20 +69,27 @@ public class BackpressureUtils {
         }
     }
 
+    private static RestClusterClient<StandaloneClusterId> createClient(
+            int port, Configuration clientConfiguration) throws Exception {
+        final Configuration clientConfig = new Configuration();
+        clientConfig.addAll(clientConfiguration);
+        clientConfig.setInteger(RestOptions.PORT, port);
+        return new RestClusterClient<>(clientConfig, StandaloneClusterId.getInstance());
+    }
+
     private static JobVertexBackPressureInfo.VertexBackPressureLevel queryBackpressure(
-            JobID jobID, JobVertexID vertexID, RestClient restClient, URI restAddress) {
+            JobID jobID, JobVertexID vertexID, RestClusterClient restClient, URI restAddress) {
         try {
             final JobVertexMessageParameters metricsParameters = new JobVertexMessageParameters();
             metricsParameters.jobPathParameter.resolve(jobID);
             metricsParameters.jobVertexIdPathParameter.resolve(vertexID);
-            return restClient
-                    .sendRequest(
-                            restAddress.getHost(),
-                            restAddress.getPort(),
-                            JobVertexBackPressureHeaders.getInstance(),
-                            metricsParameters,
-                            EmptyRequestBody.getInstance())
-                    .get()
+            return ((JobVertexBackPressureInfo)
+                            restClient
+                                    .sendRequest(
+                                            JobVertexBackPressureHeaders.getInstance(),
+                                            metricsParameters,
+                                            EmptyRequestBody.getInstance())
+                                    .get())
                     .getBackpressureLevel();
         } catch (Exception e) {
             throw new FlinkRuntimeException(e);
